85f73373b76a7fd987f45bc38a59e4fa1507de63
Santiago3 <3055242489@qq.com>
2019-03-24 16:25:57 +0800

    lexical analysis1

--- Code/main.c
+++ Code/main.c
@@ -1,1 +1,21 @@
-#
+#include <stdio.h>
+
+int main(int argc, char ** argv){
+    if (argc < 2){
+        yylex();
+        printf("Lexical analysis of one file completed!\n");
+        return 0;
+    }
+    for(int i = 1; i < argc; i++){
+        FILE *f = fopen(argv[i], "r");
+        if (!f){
+            perror(argv[i]);
+            return 1;
+        }
+        yyrestart(f);
+        yylex();
+        fclose(f);
+        printf("Lexical analysis of one file completed!\n");
+    }
+    return 0;
+}
