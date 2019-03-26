ff994b08ff8a4ae1c3e7ac0e1c485e28e61442ef
Santiago3 <3055242489@qq.com>
2019-03-26 21:26:00 +0800

    Gramma analysis1

--- Code/main.c
+++ Code/main.c
@@ -2,10 +2,13 @@
 
 int main(int argc, char ** argv){
     int i;
-    if (argc < 2){
+    /*if (argc < 2){
         yylex();
         printf("Lexical analysis of one file completed!\n");
         return 0;
+    }*/
+    if(argc < 2){
+        return 1;
     }
     for(i = 1; i < argc; i++){
         FILE *f = fopen(argv[i], "r");
@@ -14,9 +17,11 @@ int main(int argc, char ** argv){
             return 1;
         }
         yyrestart(f);
-        yylex();
+        //yylex();
+        yyparse();
         fclose(f);
-        printf("Lexical analysis of one file completed!\n");
+        //printf("Lexical analysis of one file completed!\n");
+        printf("Gramma analysis of one file completed!\n");
     }
     return 0;
 }
